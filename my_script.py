a6583e9e460a0d36de47e66fefc50adac1d94ea3
Guido Diepen <site-github@guidodiepen.nl>
2021-03-24 09:12:13 +0100

    make script depending on y.txt

--- my_script.py
+++ my_script.py
@@ -5,7 +5,9 @@ with open("params.yaml") as f:
     settings = yaml.safe_load(f)["settings"]
 
     x = settings["x"]
-    y = settings["y"]
+
+with open("y.txt") as f:
+    y = float(f.read())
 
 with open("metrics.json", "w") as f:
     f.write(json.dumps({"product": x * y}))
